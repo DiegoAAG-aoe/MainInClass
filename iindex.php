d1b77d7be63cccd6554277bfc8671419cf0b100a
juasjuasjuas <69996441+juasjuasjuas@users.noreply.github.com>
2023-08-02 12:45:23 -0400

    fdg

---
+++ iindex.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<?php include("Template/Cabecera.php"); ?>
+<head>
+    <meta charset="utf=8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Inicio Usuario</title>
+    <link rel="stylesheet" href="formatos_css/clases.css" media="screen">
+    <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+</head>
+
+    <div class="indexfondo">
+    <img src="./Logos/logoinclass.png" alt="inclass"class="logoinclass"></img>
+    </div>
+</body>
+<footer class="pie-pagina">
+<img src="./Logos/CNA7.png" alt="7anos" class="logodeabajo" ></img>
+</footer>
+
+</html>
--- index.php
+++ index.php
@@ -1,20 +1,70 @@
-<!DOCTYPE html>
-<html>
-<?php include("Template/Cabecera.php"); ?>
-<head>
-    <meta charset="utf=8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Inicio Usuario</title>
-    <link rel="stylesheet" href="formatos_css/clases.css" media="screen">
-    <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
-</head>
+<?php require 'config.php'; ?>
+ <!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head> 
+		<meta charset="utf-8">
+		<title>Import Excel To MySQL</title>
+	</head>
+	<body>
+		<form class="" action="" method="post" enctype="multipart/form-data">
+			<input type="file" name="excel" required value="">
+			<button type="submit" name="import">Import</button>
+		</form>
+		<hr>
+		<table border = 1>
+			<tr>
+				<td>#</td>
+				<td>Name</td>
+				<td>correo</td>
+				<td>contra</td>
+			</tr>
+			<?php
+			$i = 1;
+			$rows = mysqli_query($conn, "SELECT * FROM jefec");
+			foreach($rows as $row) :
+			?>
+			<tr>
+				<td> <?php echo $row["Jef_Rut"]; ?> ?> </td>
+				<td> <?php echo $row["Jef_Nombre"]; ?> </td>
+				<td> <?php echo $row["Jef_Correo"]; ?> </td>
+				<td> <?php echo $row["Jef_contrasena"]; ?> </td>
+			</tr>
+			<?php endforeach; ?>
+		</table>
+		<?php
+		if(isset($_POST["import"])){
+			$fileName = $_FILES["excel"]["name"];
+			$fileExtension = explode('.', $fileName);
+      $fileExtension = strtolower(end($fileExtension));
+			$newFileName = date("Y.m.d") . " - " . date("h.i.sa") . "." . $fileExtension;
 
-    <div class="indexfondo">
-    <img src="./Logos/logoinclass.png" alt="inclass"class="logoinclass"></img>
-    </div>
-</body>
-<footer class="pie-pagina">
-<img src="./Logos/CNA7.png" alt="7anos" class="logodeabajo" ></img>
-</footer>
+			$targetDirectory = "uploads/" . $newFileName;
+			move_uploaded_file($_FILES['excel']['tmp_name'], $targetDirectory);
+			var_dump(move_uploaded_file($_FILES['excel']['tmp_name'], $targetDirectory));
 
+			// error_reporting(0);
+			// ini_set('display_errors', 0);
+
+			require 'excelReader/excel_reader2.php';
+			require 'excelReader/SpreadsheetReader.php';
+
+			$reader = new SpreadsheetReader($targetDirectory);
+			foreach($reader as $key => $row){
+				$rut = $row[0];
+				$name = $row[1];
+				$correo = $row[2];
+				$pass = $row[3];
+				mysqli_query($conn, "INSERT INTO jefec VALUES('$rut', '$name', '$correo', '$pass')");
+			}
+
+			echo
+			"
+			<script>
+			alert('Succesfully Imported');
+			document.location.href = '';
+			</script>
+			";
+		}
+		?>
+	</body>
 </html>
